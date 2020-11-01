af8e90a1e317b4b5d1b5d96c720a6f7b8ac01824
Umakant <umakantvashishtha98@gmail.com>
2020-11-01 22:14:53 +0530

    Updates

---
+++ precache-manifest.6df9ae2461c1c4e9e769a911fabc2822.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5637c85996fd0f354db122ff3827b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af63dd203c1cc0b88ff2",
+    "url": "/static/css/2.bb4705a5.chunk.css"
+  },
+  {
+    "revision": "02324a7b5e6587fe8934",
+    "url": "/static/css/main.60375c39.chunk.css"
+  },
+  {
+    "revision": "af63dd203c1cc0b88ff2",
+    "url": "/static/js/2.ec6c9a29.chunk.js"
+  },
+  {
+    "revision": "9aae5077729947dfa90ccff7014f56af",
+    "url": "/static/js/2.ec6c9a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02324a7b5e6587fe8934",
+    "url": "/static/js/main.af7bcdf1.chunk.js"
+  },
+  {
+    "revision": "28a9b60e01bb27ab3b58",
+    "url": "/static/js/runtime-main.538f3bf5.js"
+  },
+  {
+    "revision": "7e86ef7a4ce295cb520e188523940619",
+    "url": "/static/media/Logo.7e86ef7a.png"
+  },
+  {
+    "revision": "aa3309a849a70b3cc754c9f3766cadd2",
+    "url": "/static/media/blog1.aa3309a8.jpeg"
+  },
+  {
+    "revision": "8ac3b0b62ae5126c367ad0d7e2fe9606",
+    "url": "/static/media/blog2.8ac3b0b6.jpeg"
+  },
+  {
+    "revision": "27119f35a61bebe2e38688a82f374fc3",
+    "url": "/static/media/blog3.27119f35.jpeg"
+  },
+  {
+    "revision": "c484cd5b14ed89a335911ffa9c9d612d",
+    "url": "/static/media/blog4.c484cd5b.jpeg"
+  },
+  {
+    "revision": "d767d637ecd17bc8490a95fa11dbed25",
+    "url": "/static/media/box.d767d637.svg"
+  },
+  {
+    "revision": "3e4238e2520a23b2d430576f9627de27",
+    "url": "/static/media/contact.3e4238e2.svg"
+  },
+  {
+    "revision": "7c7954aeabf23e731fd60e8d36e203e9",
+    "url": "/static/media/cow-milk.7c7954ae.svg"
+  },
+  {
+    "revision": "7d175b67a27059d2db23241a33ee70cf",
+    "url": "/static/media/cow.7d175b67.svg"
+  },
+  {
+    "revision": "7c43a95f96325d7363740403767b4c86",
+    "url": "/static/media/delivery.7c43a95f.svg"
+  },
+  {
+    "revision": "cd320ea2c7f3cb6a32a65a60b6c0b7a9",
+    "url": "/static/media/farm.cd320ea2.jpg"
+  },
+  {
+    "revision": "bc938f2d6b2d321830bfababbbd448e9",
+    "url": "/static/media/flask.bc938f2d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "40f05dbde90b90447cefa1b6dbb08c24",
+    "url": "/static/media/img1.40f05dbd.jpg"
+  },
+  {
+    "revision": "97512cce2be46b8fa7db12fe44b21e85",
+    "url": "/static/media/medal.97512cce.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "69bc5be73eb617f149e3ada6fcf73c48",
+    "url": "/static/media/packaging.69bc5be7.svg"
+  },
+  {
+    "revision": "4f6e859b3a3f165ee266271d7b411a11",
+    "url": "/static/media/planet-earth.4f6e859b.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "315b768edf9112db20b7dfa21b09f8bf",
+    "url": "/static/media/shield.315b768e.svg"
+  },
+  {
+    "revision": "969b7406df8539065b7ccf3e1e222658",
+    "url": "/static/media/shipped.969b7406.svg"
+  },
+  {
+    "revision": "3130b40ed22eb26d3bb3aa0592a31b27",
+    "url": "/static/media/test-tube.3130b40e.svg"
+  },
+  {
+    "revision": "2d00794d3abcaf472fa02f4ac4b59f3f",
+    "url": "/static/media/testing.2d00794d.svg"
+  },
+  {
+    "revision": "461089b8b31602c9c75ea2c022e55783",
+    "url": "/static/media/whatsapp-logo.461089b8.webp"
+  }
+]);
